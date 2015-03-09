00a99edd85f82012f40b75be4a6f367bde10d7f4
Michael Bond <codemonkey85@gmail.com>
2015-03-08 22:35:08 -0400

    Add SQLITE code

--- Baby Book UI Prototyping/Form1.Designer.cs
+++ Baby Book UI Prototyping/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.txtBody = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -46,21 +48,32 @@
             // 
             this.txtBody.AcceptsReturn = true;
             this.txtBody.AcceptsTab = true;
-            this.txtBody.Location = new System.Drawing.Point(12, 12);
+            this.txtBody.Location = new System.Drawing.Point(689, 534);
             this.txtBody.Multiline = true;
             this.txtBody.Name = "txtBody";
-            this.txtBody.Size = new System.Drawing.Size(1240, 538);
+            this.txtBody.Size = new System.Drawing.Size(351, 266);
             this.txtBody.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1252, 528);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1264, 942);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.txtBody);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +83,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.DataGridView dataGridView1;
 
     }
 }
